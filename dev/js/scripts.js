b113cff0c538f9cadbe97a8ad6559353a8ca3412
tc3244 <tc3244@drexel.edu>
2023-04-27 03:01:42 -0400

    tl

--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -14,3 +14,53 @@ gsap.registerPlugin(ScrollTrigger);
 // gsap.from("#content h1", {scrollTrigger: {trigger: "content h1",toggleActions:"restart pause reverse none"}, duration:.5, alpha:0, x:"-=50", delay:1})
 // gsap.from("#content p",{scrollTrigger:{trigger: "content p", toggleActions:"restart pause reverse none"}, stagger:0.2, duration:.5, alpha:0, x:"-=50", delay:.5})
 
+
+let mainTl = gsap.timeline({delay:1});
+
+
+function heroAni(){
+
+    let tl = gsap.timeline({delay:1});
+    let mm = gsap.matchMedia();
+
+    mm.add("(max-width:767px)", () =>{
+    tl.from("#logo",{duration:2.5, alpha:0, delay:0.5, x:"-=200"})
+    .from("nav li",{duration:1, alpha:0, y:"-=25", delay:0.5,x:"+=200"})
+    .from("#hero article",{duration:3, alpha:0, delay:0.5})
+    .from("#hero h1",{duration:.75, alpha:0,y:"-=200", delay:3})
+    .from("#hero h2",{duration:.5, alpha:0,y:"-=200", delay:3})
+    .from("#hero button",{duration:.5, alpha:0,y:"+=200", delay:3})
+    ;
+    });
+
+    mm.add("(min-width:768px)", () =>{
+        tl.from("#logo",{duration:2.5, alpha:0, delay:0.5, y:"-=200"})
+        .from("nav li",{duration:1, alpha:0, x:"-=25", delay:0.5,y:"+=200"})
+        .from("#hero article",{duration:3, alpha:0, delay:0.5})
+        .from("#hero h1",{duration:.75, alpha:0, x:"-=200", delay:3})
+        .from("#hero h2",{duration:.5, alpha:0, x:"-=200", delay:3})
+        .from("#hero button",{duration:.5, alpha:0, x:"+=200", delay:3})
+        ;
+        });
+
+    return tl;
+}
+
+function bodyAni(){
+
+    let tl = gsap.timeline({delay:1});
+
+    tl.from("#content h1", {scrollTrigger: {trigger: "content h1",toggleActions:"restart pause reverse none"}, duration:.5, alpha:0, x:"-=50", delay:1})
+    .from("#content p",{scrollTrigger:{trigger: "content p", toggleActions:"restart pause reverse none"}, duration:.5, alpha:0, x:"-=50", delay:.5})
+    
+    ;
+
+    return tl;
+}
+
+
+
+mainTl.add(heroAni())
+.add(bodyAni())
+
+;
